9a5450c24fabf22eeacededf0bb553372c55f110
Arpit Pandey <arpit.pandey@taazaa.com>
2021-05-10 12:12:23 +0530

    initialized solution with problem's name

---
+++ kata 2/Sum of differences between products and LCMs/Sum of differences between products and LCMs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sum_of_differences_between_products_and_LCMs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ kata 3/Enough is enough/Enough is enough/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enough_is_enough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
