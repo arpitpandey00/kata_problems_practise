dffbdb65c5ce20df30335e69dfe787c6cacf74ab
Arpit Pandey <arpit.pandey@taazaa.com>
2021-05-12 10:56:01 +0530

    Kata problem statement 4

--- kata 4/Sum consecutives/Sum consecutives/EntryAndLogic.cs
+++ kata 4/Sum consecutives/Sum consecutives/EntryAndLogic.cs
@@ -15,33 +15,25 @@ namespace Sum_consecutives
             int[] array = new int[length];
             List<int> array2 = new List<int> ();
             Console.WriteLine("Enter elements of array");
+
+            
             for (int i=0;i< length;i++)
             {
                 array[i]= Convert.ToInt32(Console.ReadLine());
             }
-            for(int i=0;i< length-1;i++)
+            for (int i = 0; i < array.Length  ; i++)
             {
-                if (array[i] == array[i + 1])
+                int temp=array[i];
+                for( int j=i+1;j<array.Length;j++)
                 {
-                    int j = i;
-                    int temp = array[j];
-                    while (array[j] == array[j + 1])
+                    if(array[j]==array[i])
                     {
-                        temp += array[j + 1];
-                        j++;
+                        temp += array[j];
+                        i = j;
                     }
-                    array2.Add(temp);
-                    i = j;
+                   
                 }
-                else
-                {
-                    array2.Add(array[i]);
-                        
-                };
-            }
-            if ((array[length - 2] != array[length-1]))
-            {
-                array2.Add(array[length - 1]);
+                array2.Add(temp);
             }
             array2.ForEach(element => Console.WriteLine(element));
         }
